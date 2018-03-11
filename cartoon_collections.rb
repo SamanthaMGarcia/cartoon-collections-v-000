e9c0ce3f48b5b35147dd15d017bfbd6471324623
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 13:32:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call| "call", + "!" 
+  planeteer_calls.collect do |call| call + "!" 
     call.capitalize
     planeteer_calls.join(""",")
   end
