fad03cd2de850e4c37f2feb5cee45db3f0645896
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 13:27:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   new_array = [] 
-  planeteer_calls.collect do |call|
+  planeteer_calls.collect do |call| call + ! 
     call.capitalize
     planeteer_calls.join(""",")
   puts "#{call[0]}! #{call[1]}! #{call[2]}!"
