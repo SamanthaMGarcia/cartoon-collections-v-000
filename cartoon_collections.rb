0e1ff5884f96d257ec1272cc439d38274d5a97e8
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 15:34:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.include?("cheddar"|| "gouda"|| "camembert" }
+  strings.find_any("cheddar"|| "gouda"|| "camembert" }
 end
 
