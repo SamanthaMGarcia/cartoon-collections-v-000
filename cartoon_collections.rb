402bc27de5e793056850db1e36d1e63f3d9337ba
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 14:15:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.find_all? { |call| call.length > 4 }
+  planeteer_calls.find_all { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
