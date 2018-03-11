9986730fc49b05d7371b9fad1ad8c5a2faf99e0c
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-11 15:43:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(cheese_types)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find { |cheese| cheese == "cheddar"|| "gouda"|| "camembert" }
 end
 
